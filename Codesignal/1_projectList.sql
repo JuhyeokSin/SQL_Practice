5b12d5d19ece739d73d99031d44472722797c885
JuhyeokSin <juhyeok.sin@sjsu.edu>
2019-11-26 10:41:04 -0800

    adding a modification for 1_projectList.sql

--- Codesignal/1_projectList.sql
+++ Codesignal/1_projectList.sql
@@ -29,11 +29,11 @@ FROM Projects
 ORDER BY internal_id ASC;
 =======
 /* ProjectList
-Your boss wants to identify the successful projects running in your company, 
-so he asked you to prepare a list of all the currently active projects and 
+Your boss wants to identify the successful projects running in your company,
+so he asked you to prepare a list of all the currently active projects and
 their average monthly income.
 
-You have stored the information about these projects in a simple database with 
+You have stored the information about these projects in a simple database with
 a single Projects table that has five columns:
 
 - internal_id: the company's internal identifier for the project;
@@ -42,11 +42,11 @@ a single Projects table that has five columns:
 - team_lead: the name of the project manager;
 - income: the average monthly income of the project.
 
-create another table by removing the internal_id and team_size columns from the 
+create another table by removing the internal_id and team_size columns from the
 existing Projects table. Return it sorted by internal_id in ascending order.
 */
 
-SELECT project_name, team_lead, income 
+SELECT project_name, team_lead, income
 FROM Projects
 ORDER BY internal_id ASC;
 >>>>>>> 9b4e3a0d779bd11626585aad53ea4bfa06ac3e66
