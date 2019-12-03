dd6cda9169bba8e42da9e058f3251afca8ff1fe8
JuhyeokSin <juhyeok.sin@sjsu.edu>
2019-12-03 10:01:59 -0800

    adding a solved problem

--- Codesignal/27_combinationLock.sql
+++ Codesignal/27_combinationLock.sql
@@ -8,3 +8,12 @@ The combination lock consists of several rotating discs, where each disc has its
 
 Calculate the total number of all possible combinations that the lock has, and return it as a table that has only one column combinations and one row.
 */
+SELECT ROUND(EXP(SUM(LOG(LENGTH(characters)))),0) AS combinations
+FROM discs;
+
+/*
+1. convert all values in the column in logarithmic value
+2. sum all of the log values (sum of log gives a multiplication all over exponents)
+3. exponentiate the logarithmic sum, which gives a value that we are looking for
+4. round to the integer
+*/
