02e956ad3b8e93a478147b77374191d08b8170d0
JuhyeokSin <juhyeok.sin@sjsu.edu>
2019-12-16 14:43:18 -0800

    adding the solved problems, #1, #2, and #3

---
+++ SQLZOO/The_Join_operation.sql
@@ -0,0 +1,45 @@
+/*
+This tutorial introduces JOIN which allows you to use data from two or more tables. The tables contain all matches and goals from UEFA EURO 2012 Football Championship in Poland and Ukraine.
+
+The data is available (mysql format) at http://sqlzoo.net/euro2012.sql
+*/
+
+/****** Problems ******/
+
+/* 1.
+The first example shows the goal scored by a player with the last name 'Bender'.
+The * says to list all the columns in the table - a shorter way of saying matchid, teamid, player, gtime
+
+Modify it to show the matchid and player name for all goals scored by Germany. To identify German players,
+check for: teamid = 'GER'
+*/
+
+SELECT matchid, player
+FROM goal
+WHERE teamid='GER'
+
+/* 2.
+From the previous query you can see that Lars Bender's scored a goal in game 1012. Now we want to know what teams were playing in that match.
+
+Notice in the that the column matchid in the goal table corresponds to the id column in the game table. We can look up information about game 1012 by finding that row in the game table.
+
+Show id, stadium, team1, team2 for just game 1012
+*/
+
+SELECT id, stadium, team1, team2
+FROM game
+WHERE id=1012
+
+/* 3.
+The FROM clause says to merge data from the goal table with that from the game table. The ON says how to figure out which rows in game go
+with which rows in goal - the matchid from goal must match id from game.
+(If we wanted to be more clear/specific we could say
+  ON (game.id=goal.matchid)
+
+The code below shows the player (from the goal) and stadium name (from the game table) for every goal scored.
+
+Modify it to show the player, teamid, stadium and mdate for every German goal.
+*/
+SELECT player, teamid, stadium, mdate
+FROM game JOIN ON (id=matchid)
+WHERE teamid='GER'
